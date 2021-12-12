5c80e77b917e3ba75365343764565c9c364b3aa6
Muhannad Alghamdi <MuhannadAlghamdi@outlook.com>
2021-12-12 16:00:32 +0300

    making the base ready

--- app/src/main/java/sa/edu/tuwaiq/tsuki/View/MainActivity.kt
+++ app/src/main/java/sa/edu/tuwaiq/tsuki/View/MainActivity.kt
@@ -1,7 +1,8 @@
-package sa.edu.tuwaiq.tsuki
+package sa.edu.tuwaiq.tsuki.View
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import sa.edu.tuwaiq.tsuki.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
